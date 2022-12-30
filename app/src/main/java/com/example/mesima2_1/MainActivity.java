dd56d88ec83ecf5ec2377fa5edf0a6f1a4a27971
Noam <noamba1305@gmail.com>
2022-12-30 22:25:22 +0200

    a porgram where you touch a button

---
+++ app/src/main/java/com/example/mesima2_1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mesima2_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+    }
+
+    public void Clicker(View view) {
+        btn.setText("Oh, yea, I've been clicked!");
+    }
+}
